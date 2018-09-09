15793efd6c7c44601b6b69e5a5dc588aa8366fbf
aniruddha mahapatra <aniruddha26398@gmail.com>
2018-09-09 14:19:02 +0530

    Update read_img_1.py

--- Src/read_img_1.py
+++ Src/read_img_1.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 #IMREAD_GRAYSCALE
 #IMREAD_COLOR
 #IMREAD_UNCHANGED
-img = cv2.imread('/home/aniruddha/Documents/Aries/Workshops/Workshop1_Image_Processing/Data/lenna.jpg',cv2.IMREAD_GRAYSCALE)
+img = cv2.imread('../Data/lenna.jpg',cv2.IMREAD_GRAYSCALE)
 print(img.shape)
 cv2.imshow('image',img)
 cv2.waitKey(0)
