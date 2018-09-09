2c6aea2bd82a47065f49f83227507490928bd36d
aniruddha mahapatra <aniruddha26398@gmail.com>
2018-09-09 14:21:32 +0530

    correct img bugs

--- Src/threshold_5.py
+++ Src/threshold_5.py
@@ -1,7 +1,7 @@
 
 '''import cv2
 import numpy as np
-img = cv2.imread('/home/aniruddha/Documents/Aries/Workshops/Workshop1_Image_Processing/Data/bookpage.jpg')
+img = cv2.imread('../Data/bookpage.jpg')
 grayscaled = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 retval, threshold = cv2.threshold(grayscaled, 10, 255, cv2.THRESH_BINARY)
 cv2.imshow('original',img)
@@ -11,7 +11,7 @@ cv2.destroyAllWindows()'''
 
 import cv2
 import numpy as np
-img = cv2.imread('/home/aniruddha/Documents/Aries/Workshops/Workshop1_Image_Processing/Data/bookpage.jpg')
+img = cv2.imread('../Data/bookpage.jpg')
 grayscaled = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 th = cv2.adaptiveThreshold(grayscaled, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 1)
 cv2.imshow('original',img)
